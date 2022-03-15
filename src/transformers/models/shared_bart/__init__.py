8859aca95eb826bcba6325f5c8f59d7b28925fa5
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2022-03-15 12:00:20 +0700

    Update __init__.py

--- src/transformers/models/shared_bart/__init__.py
+++ src/transformers/models/shared_bart/__init__.py
@@ -21,7 +21,7 @@ from ...file_utils import _LazyModule, is_flax_available, is_tf_available, is_to
 
 
 _import_structure = {
-    "configuration_bart": ["BART_PRETRAINED_CONFIG_ARCHIVE_MAP", "BartSharedConfig", "BartSharedOnnxConfig"],
+    "configuration_bart": ["BART_PRETRAINED_CONFIG_ARCHIVE_MAP", "BartConfig", "BartOnnxConfig"],
     "tokenization_bart": ["BartTokenizer"],
 }
 
@@ -41,7 +41,7 @@ if is_torch_available():
     ]
 
 if TYPE_CHECKING:
-    from .configuration_bart import BART_PRETRAINED_CONFIG_ARCHIVE_MAP, BartSharedConfig, BartSharedOnnxConfig
+    from .configuration_bart import BART_PRETRAINED_CONFIG_ARCHIVE_MAP, BartConfig, BartOnnxConfig
     from .tokenization_bart import BartTokenizer
 
     if is_tokenizers_available():
